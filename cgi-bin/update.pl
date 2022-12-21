892ec74b755abf5878ac0b7c1ba654226f327ead
JeanChara <jcharaco@unsa.edu.pe>
2022-12-21 16:49:27 -0500

    añadido funcionalidad a update.pl

--- cgi-bin/update.pl
+++ cgi-bin/update.pl
@@ -8,68 +8,40 @@ use warnings;
 ## calculamos value de cuerpo
 
 my $q = CGI->new;
+my $owner = $q->param('usuario');
 my $titulo = $q->param('titulo');
+my $markdown = $q->param('cuerpo');
 
 my $user = 'alumno';
 my $password = 'pweb1';
 my $dsn = "DBI:MariaDB:database=pweb1;host=192.168.1.5";
 my $dbh = DBI->connect($dsn, $user, $password) or die("No se pudo conectar!");
 
-my $sth = $dbh->prepare("SELECT markdown FROM wiki WHERE nombrePag=?");
-$sth->execute($titulo);
+my $sth = $dbh->prepare("SELECT markdown FROM Articles WHERE owner=? AND title=?");
+$sth->execute($owner,$titulo);
+my @row = $sth->fetchrow_array;
 
-my @texto;
-while (my @row = $sth->fetchrow_array){
-  push (@texto,@row);
+print $q->header('text/XML');
+print "<?xml version='1.0' encoding='utf-8'?>\n";
+
+if (!(@row == 0)){
+	# si existe una pagina con el nombre y titulo dado...
+
+	$sth = $dbh->prepare ("UPDATE Articles SET markdown=? WHERE title=? AND owner=?");
+	$sth->execute($markdown,$titulo,$owner);
+
+	$sth->finish;
+
+	print "<article>";
+	print "<tittle> $titulo </tittle>";
+	print "<text> $markdown </text>";
+	print "</article>";
 }
-
-$sth->finish;
+else {
+	print "<article>";
+	print "<tittle></tittle>";
+	print "<text></text>";
+	print "</article>";
+	
+}
 $dbh->disconnect;
-
-print "Content-type: text/html\n\n";
-print <<HTML;
-
-<html>
-	<head>
-		<link rel="icon" href="../icon/UNSA.ico">
-		<link rel = "stylesheet" href = "../css/estilos.css" >
-		<title> Editando Pagina</title>
-		<div = style="text-align: center;">
-			<img src = "../img/logoUNSA.png" width = "500" height = "227" alt = "Logo UNSA">
-			<address class="autor">
-				<p>Autor: Jean Carlo Chara Condori.</p>
-			</address>
-		</div>
-	</head>
-
-	<body >
-		<div style="text-align: center;">
-			<p>Editando pagina - markdown</p><br><br>
-			<a href="../paginaPrincipal.html"  class="boton" >Cancelar</a> <br><br><br>
-
-			<h1>$titulo</h1>
-			 
-			<form style="margin-top: 30px;" method = "GET" action="new.pl">
-
-				<textarea class = "cuerpo" type="text" name="cuerpo">@texto</textarea><br>
-				
-				<input class="boton" id ="bordes1" style="background-color: black;" type="submit" value="Enviar">
-				<input type = "hidden" name = "titulo" value = "$titulo">
-
-			</form>
-		</div>
-	</body>
-
-	<script>
-		setInterval(bordesAleatorios,1000);
-		function bordesAleatorios(){
-			var x = Math.floor(Math.random() * 256);
-			var y = Math.floor(Math.random() * 256);
-			var z = Math.floor(Math.random() * 256);
-			var color = "rgb(" + x + "," + y + "," + z + ")";
-			document.getElementById('bordes1').style.border = "thick solid "+color;
-		}
-	</script>
-</html>
-
-HTML
